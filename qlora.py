9cc3b1f88b3401b3df8a37d49bce0b150662e75f
yrc40 <yiruchen8266.mg10@nycu.edu.tw>
2025-03-26 01:07:10 +0800

    gaihw2

--- qlora.py
+++ qlora.py
@@ -295,7 +295,7 @@ def get_accelerate_model(args, checkpoint_dir):
         
     max_memory = f'{args.max_memory_MB}MB'
     max_memory = {i: max_memory for i in range(n_gpus)}
-    device_map = "auto"
+    device_map = {'': 0}
 
     # if we are in a distributed setting, we need to set the device map and max memory per device
     if os.environ.get('LOCAL_RANK') is not None:
